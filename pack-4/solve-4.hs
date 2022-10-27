7b4566d2135af4949c2d0d3fb13869d56638fa4c
daria-barsukova <115018770+daria-barsukova@users.noreply.github.com>
2022-10-27 17:20:08 +0700

    Update solve-4.hs

--- pack-4/solve-4.hs
+++ pack-4/solve-4.hs
@@ -1,3 +1,4 @@
+import Data.Char
 bin:: Int -> [Int]
 bin x | x <= 1 = [x]
       | otherwise = bin (x `div` 2) ++ [x `mod` 2]
@@ -6,7 +7,11 @@ dec:: [Int] -> Int -> Int
 dec x system = foldr (\q acc -> (acc * system + q)) (last (reverse x)) (init (reverse x))
 
 str:: String -> Int
-str x = read x
+str = helper 0
+    where helper c [] = c
+          helper c (x:xs) | 0 <= ch && ch <= 9 = helper (10 * c + ch) xs
+                          | otherwise = 0
+              where ch = digitToInt x
 
 func:: Int -> [Int] -> [Int]
 func x [] = [x]
@@ -18,5 +23,8 @@ sort (x:xs) = func x (sort xs)
 
 lost:: [Int]->Int
 lost m = helper (sort m)
-      where helper lst | ((head(tail lst) - head lst) > 1) = (head lst) + 1
-                       | otherwise = helper (tail lst)
+      where helper lst | (z - y > 1) = y + 1
+                       | otherwise = helper x
+                  where x = tail lst 
+                        y = head lst
+                        z = head(x)
